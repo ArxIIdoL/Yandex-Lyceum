52e51e5979c47b7b5d24e7f387ad17658981d223
ArxIIdoL <aleksandrsergvol@gmail.com>
2025-04-12 21:15:47 +0300

    Add files via upload

---
+++ WEB. Введение во flask. Обработка HTML-форм/Отбор астронавтов/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Отбор астронавтов"
+
+
+@app.route("/astronaut_selection", methods=("POST", "GET"))
+def astronaut_selection():
+    if request.method == "GET":
+        return render_template("astronaut_selection.html")
+    elif request.method == "POST":
+        return "OK"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
